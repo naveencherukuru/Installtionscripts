747e2ba18cb836d70f8b6d3ee00fba939f764195
naveencherukuru <34143195+naveencherukuru@users.noreply.github.com>
2018-01-08 13:00:31 -0800

    Add files via upload

--- Install_ScyllaDB.sh
+++ Install_ScyllaDB.sh
@@ -1,22 +1,19 @@
 sudo curl -o /etc/apt/sources.list.d/scylla.list -L http://repositories.scylladb.com/scylla/repo/ac074594c7286bea642e577ba06e721a/ubuntu/scylladb-2.0-xenial.list
 sudo apt-get update --allow-unauthenticated
 yes Y|sudo apt-get install scylla --allow-unauthenticated
-echo "Yes
-Yes
-Yes
-Yes
-Yes
-Yes
-Yes
-/dev/sdc
-done
-Yes
-Yes
+echo "yes
+yes 
+yes
+yes
+yes
+yes
+yes
+yes
+yes
 no
-Yes
-no
-Yes"| sudo scylla_setup
+yes
+yes
+yes" | sudo scylla_setup
 
 sudo systemctl start scylla-server
 nodetool status
-cassandra-stress write -mode cql3 native 
